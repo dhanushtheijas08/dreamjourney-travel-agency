78da0b968015ebd49aa26d8b71c27b44f717930b
dhanushtheijas <111133555+dhanushtheijas08@users.noreply.github.com>
2023-06-21 22:36:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import Service from './Components/Service/Service';
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Service />
-      <Destinations />
-      <PhotoGallery />
+      
     </div>
   );
 }
